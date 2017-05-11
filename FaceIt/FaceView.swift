f555d537faa175e585190427f7f92c2b2132e544
TerrillT5 <t.thorne96@yahoo.com>
2017-05-11 15:18:25 -0400

    Inspectable view added

--- FaceIt/FaceView.swift
+++ FaceIt/FaceView.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
+@IBDesignable //this shows the view instantly
+
 class FaceView: UIView {
 
+    @IBInspectable
     var scale: CGFloat = 0.9
-    
+   
+    @IBInspectable
     var eyesOpen: Bool = true // makes the eyes circular also
     
+    @IBInspectable
+    var lineWidth: CGFloat = 5.0
+    
+    @IBInspectable
     var mouthCurvature: Double = 1.0 // 1.0 is full smile & -1.0 is full frown
+    
+    @IBInspectable
+    var color: UIColor = .blue
+    
     
     private var skullRadius: CGFloat {
         return min(bounds.size.width,bounds.size.height) / 2 * scale // sets the height & width of the skull
@@ -40,51 +52,63 @@ class FaceView: UIView {
             return eyeCenter // returns center of the eye
             
             }
+      
         let eyeRadius = skullRadius / Ratios.skullRadiusToEyeRadius
         let eyeCenter = centerOfEye(eye)
         
         let path: UIBezierPath
         if eyesOpen {
             path = UIBezierPath(arcCenter: eyeCenter, radius: eyeRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true ) // makes the eyes circular
-            
         } else {
-        path = UIBezierPath()
+            path = UIBezierPath()
             path.move(to: CGPoint(x: eyeCenter.x - eyeRadius, y: eyeCenter.y))
             path.addLine(to: CGPoint(x: eyeCenter.x + eyeRadius, y: eyeCenter.y))
         }
-        path.lineWidth = 5.0 // size of the inner eye(pupil)
+        path.lineWidth = lineWidth // size of the inner eye(pupil)
         return path
         
     }
     
     private func pathForSkull() -> UIBezierPath{
         let path = UIBezierPath(arcCenter: skullCenter, radius: skullRadius, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: false) // makes the skull circular
-        path.lineWidth = 5.0 // how wide the skull will be
+        path.lineWidth = lineWidth // how wide the skull will be
         return path
         
     }
 
     private func pathForMouth() -> UIBezierPath {
         
-        let mouthWidth = skullRadius / Ratios.skullRadiusToMouthWidth
-        let mouthHeight = skullRadius / Ratios.skullRadiusToMouthHeight
+        let mouthWidth = skullRadius / Ratios.skullRadiusToMouthWidth // width of the mouth
+        let mouthHeight = skullRadius / Ratios.skullRadiusToMouthHeight // height of the mouth
         let mouthOffSet = skullRadius / Ratios.skullRadiusToMouthOffSet
         
         let mouthRect = CGRect(
-            x: skullCenter.x - mouthWidth / 2,
-            y: skullCenter.y + mouthOffSet,
+            x: skullCenter.x - mouthWidth / 2,  // location of mouth along x axis in skull. Higher the #, closer to the edge of the mouth
+            y: skullCenter.y + mouthOffSet,     // location of mouth along y axis in skull
             width: mouthWidth,
             height: mouthHeight
         )
-            let start = CGPoint(x: mouthRect.minX, y: mouthRect.minY)
-            let path = UIBezierPath(rect: mouthRect)
+        let smileOffset = CGFloat(max(-1,min(mouthCurvature,1))) * mouthRect.height // converted double(mouthCurvature) to a float
         
-            return path
+        let start = CGPoint(x: mouthRect.minX, y: mouthRect.midY) // min.x is halfway edge & mid.y is halfway up
+        let end = CGPoint(x: mouthRect.maxX, y: mouthRect.midY)
+       
+        let cp1 = CGPoint(x: start.x + mouthRect.width / 5, y: start.y + smileOffset) // starting curve of mouth
+        let cp2 = CGPoint(x: end.x - mouthRect.width / 5, y: start.y + smileOffset) // ending curve of mouth
+        
+        
+        let path = UIBezierPath()
+        path.move(to: start)
+        path.addCurve(to: end, controlPoint1: cp1, controlPoint2: cp2)
+        
+        path.lineWidth = lineWidth
+        
+        return path
     }
     
     override func draw(_ rect: CGRect) {
         
-        UIColor.blue.set() // sets the stroke color
+        color.set() // sets the stroke color 
         pathForSkull().stroke() // creates the head
         pathForEye(.left).stroke() // creates left eye
         pathForEye(.right).stroke() // creates right eye
